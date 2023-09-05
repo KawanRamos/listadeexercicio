22fee8e9a4d43fbfdb6de76feb14a12ed9a83438
KAWAN HERBERT FERREIRA RAMOS <kawan.0946@aluno.pr.senac.br>
2023-09-05 16:45:39 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/exercicios1/Exercicios1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicios1;
+import java.util.Scanner;
+/**
+ *
+ * @author kawan.0946
+ */
+public class Exercicios1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int opcao;
+        
+        float num1, num2, num3, resultado;
+        
+        System
+    }
+}
